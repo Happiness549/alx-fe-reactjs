cd7649fb5542a3bdd1fc7ed6c3d7b50531141387
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-08-26 11:16:44 +0200

    Added Footer.jsx file, Header.jsx file and MainContent.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import Header from './components/Header';
 
 function App() {
     return (
         <div>
+            <Header />
+            < MainContent />
+            <Footer />
             <WelcomeMessage />
         </div>
     );
