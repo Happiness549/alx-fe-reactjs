a43f81cddf88a8b3c8b0c2c451906a743b96b900
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-08-26 10:43:37 +0200

    Rectified welcomemessage.jsx content

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
