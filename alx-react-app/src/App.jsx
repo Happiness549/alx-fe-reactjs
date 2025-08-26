6c2d0edfb5a9b052284ea4230f7156b2dbc9c92b
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-08-26 11:33:49 +0200

    Added UserProfile.jsx file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <MainContent />
             <Footer />
             <WelcomeMessage />
+            <UserProfile name="Happiness" age="32" bio="Loves coding"/>
         </div>
     );
 }
