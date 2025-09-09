ac2866428d5177e9b968ded9923d9710de451217
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-09-09 11:13:05 +0200

    Added recipe-sharing-app directory

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import RecipeList from './components/RecipeList'
+import RecipeForm from './components/RecipeForm'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        < RecipeList />
+        < RecipeForm />
+    
+      </div>
+     
+    </>
+  )
+}
+
+export default App
