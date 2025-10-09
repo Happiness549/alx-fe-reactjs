4bc3e96a180408d25edff4b41d17a05928c2d231
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-10-09 11:17:52 +0200

    Add useAuth.js

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,16 @@
+
+import React from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
-const isAuthenticated = () => {
-      return localStorage.getItem("authToken") !== null;
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 };
 
-const PrivateRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
+export default ProtectedRoute;
