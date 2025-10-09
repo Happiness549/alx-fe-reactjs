9f9475966001751c2ae109c329bed5a41920b916
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-10-09 11:04:27 +0200

    Rectified profile component

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,20 +1,48 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails.jsx";
+import ProfileSettings from "./ProfileSettings.jsx";
 
-function Profile(){
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
 
-    return(
-        <div>
-            <h1>Profile</h1>
-            <nav>
-                <Link to ="details">Profile Details</Link>
-                <Link to="settings">Profile Settings</Link>
-            </nav>
+      
+      <nav>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
+      </nav>
 
-            <outlet/>
-        </div>
-         
-    )
-   
+      
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Profile;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
